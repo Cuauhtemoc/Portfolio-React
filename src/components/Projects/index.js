006bd79179b59475aee1d11a477f50f5d707902d
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-07-24 17:42:25 -0500

    cards added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,7 +5,7 @@ import theDipImg from './thedip.jpg'
 import reactGameImg from './reactgame.jpg';
 import liriImage from './concert-this.png';
 import overplannerImage from './googlemap.png';
-import bamazonImage from '/bamazonExample.png';
+import bamazonImage from './bamazonExample.png';
 
 class Projects extends Component{
     render(){
@@ -15,10 +15,10 @@ class Projects extends Component{
                 <div className="row">
                     <Card projectDescription="A community building app targeting refugee communities within Chicago and connecting them with local families to help facilitate a smoother transition." projectName="commUnity" herokuLink="https://community-chicago.herokuapp.com/" githubLink="https://github.com/Cuauhtemoc/commUnity" imgPath={communityImg}/>
                     <Card projectDescription="A party planning app. Users can create lists of items to share with their guests. Guests can then swipe right or left on the items indicating if they will be bringing it the the party" projectName="Who's Bringing the Dip" herokuLink="http://thedip.herokuapp.com/" githubLink="https://github.com/Cuauhtemoc/The-dip" imgPath={theDipImg}/>
-                    <Card projectDescription="A memory game that keeps track on the images selected by the user. Written using React 16" projectName ="React Game" herokuLink="https://cuauhtemoc.github.io/clicky-game/" githubLink="https://github.com/Cuauhtemoc/clicky-game" imgPath={reactGameImg}/>
-                    <Card projectDescription="" projectName="" herokuLink="" githubLink ="" imgPath=""/>
-                    <Card projectDescription="" projectName="" herokuLink="" githubLink ="" imgPath=""/>
-                    <Card projectDescription="" projectName="OverPlanner" herokuLink="" githubLink ="" imgPath=""/>
+                    <Card projectDescription="A memory game that keeps track on the images selected by the user. Written using React 16" projectName ="React Memory Game" herokuLink="https://cuauhtemoc.github.io/clicky-game/" githubLink="https://github.com/Cuauhtemoc/clicky-game" imgPath={reactGameImg}/>
+                    <Card projectDescription="A CLI node.js app using mysql to interact with the inventory of a mock store" projectName="Store Inventory Simulator" githubLink ="https://github.com/Cuauhtemoc/bamazon" imgPath={bamazonImage}/>
+                    <Card projectDescription="A command line app that provides information on songs, concert dates/venues, and movies using the Spotify, Bands in town, OMDB Apis." projectName="Node CLI Application" githubLink = "https://github.com/Cuauhtemoc/liri-node-app" imgPath={liriImage}/>
+                    <Card projectDescription="A walking route oriented schedule generator using GoogleMaps API" projectName="OverPlanner" githubLink ="https://github.com/Cuauhtemoc/OverPlanner" imgPath={overplannerImage}/>
                 </div>
             </div>
         )
